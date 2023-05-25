ca494e8c61cca9d404686c9fc12b9e0082e91ae2
Jinwoo <zvzv9808@gmail.com>
2023-05-25 23:05:51 +0900

    feat: finall repo

--- src/main/java/yjhb/meeti/repository/UserRepository.java
+++ src/main/java/yjhb/meeti/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package yjhb.meeti.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import yjhb.meeti.entity.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u")
+    List<User> findAll();
 }
