5d2eb31f3d7045368aade54e2e235950f264dd69
Jinwoo <zvzv9808@gmail.com>
2023-07-02 15:35:46 +0900

    fix: validate token before update User

--- src/main/java/yjhb/meeti/api/update/controller/UpdateController.java
+++ src/main/java/yjhb/meeti/api/update/controller/UpdateController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import yjhb.meeti.api.update.dto.UpdateDTO;
 import yjhb.meeti.api.update.service.UpdateService;
+import yjhb.meeti.global.jwt.service.TokenManager;
 import yjhb.meeti.global.resolver.memberinfo.UserInfo;
 import yjhb.meeti.global.resolver.memberinfo.UserInfoDto;
 import yjhb.meeti.global.util.AuthorizationHeaderUtils;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 public class UpdateController {
 
     private final UpdateService updateService;
+    private final TokenManager tokenManager;
 
     @PostMapping("/update")
     public ResponseEntity<String> updateUser(@RequestBody UpdateDTO updateDTO,
@@ -25,6 +27,10 @@ public class UpdateController {
 
         String authorization = httpServletRequest.getHeader("Authorization");
         AuthorizationHeaderUtils.validateAuthorization(authorization);
+
+        String accessToken = authorization.split(" ")[1];
+        // 토큰 유효성 체크
+        tokenManager.validateToken(accessToken);
 
         updateService.updateUser(userInfoDto.getId(), updateDTO.getUsername(), updateDTO.getProfile());
 
