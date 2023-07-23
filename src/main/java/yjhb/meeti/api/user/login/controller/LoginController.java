d4d0e68b7b184d057d83015a3a03e8ab2571b529
Jinwoo <zvzv9808@gmail.com>
2023-07-23 22:43:26 +0900

    docs: add Swagger Info at Response Schema

--- src/main/java/yjhb/meeti/api/user/login/controller/LoginController.java
+++ src/main/java/yjhb/meeti/api/user/login/controller/LoginController.java
@@ -1,5 +1,7 @@
 package yjhb.meeti.api.user.login.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,19 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yjhb.meeti.api.user.login.dto.LoginDTO;
 import yjhb.meeti.api.user.login.service.LoginService;
-import yjhb.meeti.api.user.login.validate.Validator;
-import yjhb.meeti.domain.user.service.UserService;
 
+@Tag(name = "Login", description = "User Login API") // API에 대한 구체적 설명
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/meeti/user")
 public class LoginController {
-
-    private final Validator validator;
     private final LoginService loginService;
-    private final UserService userService;
 
-
+    @Tag(name = "Login")
+    @Operation(summary = "User Login API", description = "유저 서비스를 활용하기 위한 로그인 서비스 API")
     @PostMapping("/login")
     public ResponseEntity<LoginDTO.Response> login(@RequestBody LoginDTO.Request requestDto){
 
--- src/main/java/yjhb/meeti/api/user/login/dto/LoginDTO.java
+++ src/main/java/yjhb/meeti/api/user/login/dto/LoginDTO.java
@@ -1,6 +1,7 @@
 package yjhb.meeti.api.user.login.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import yjhb.meeti.global.jwt.dto.JwtTokenDto;
 
@@ -17,11 +18,17 @@ public class LoginDTO {
     @Getter @Setter
     @Builder @NoArgsConstructor @AllArgsConstructor
     public static class Response{
+
+        @Schema(description = "인증 방식을 정의하는 타입 (JWT/OAuth 부여 방식 : Bearer)", example = "Bearer", required = true)
         private String grantType;
+        @Schema(description = "User Access Token", required = true)
         private String accessToken;
+        @Schema(description = "User Access Token 만료 시간", example = "2023-07-23 20:55:00", required = true)
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         private Date accessTokenExpireTime;
+        @Schema(description = "Access Token이 만료된 후, 재발급하기 위한 User Refresh Token", required = true)
         private String refreshToken;
+        @Schema(description = "User Refresh Token 만료 시간", example = "2023-07-23 20:55:00", required = true)
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         private Date refreshTokenExpireTime;
 
