941ad6ad1de138565136cee389d549266fab8290
Jinwoo <zvzv9808@gmail.com>
2023-09-10 16:08:21 +0900

    feat: Delete All Meetings of User

--- src/main/java/yjhb/meeti/api/meeting/controller/MeetingController.java
+++ src/main/java/yjhb/meeti/api/meeting/controller/MeetingController.java
@@ -1,5 +1,7 @@
 package yjhb.meeti.api.meeting.controller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import yjhb.meeti.global.jwt.service.TokenManager;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+@Tag(name = "Meeting", description = "회의록 API")
 @RestController
 @RequestMapping("meeti/meeting")
 @RequiredArgsConstructor
@@ -24,6 +27,7 @@ public class MeetingController {
 
     private final TokenManager tokenManager;
 
+    @Schema(name = "Register Meeting", description = "회의록 등록 API")
     @PostMapping("/reg/{userId}")
     public ResponseEntity<Boolean> createMeeting(@RequestBody MeetingDto.Request dto,
                                                  @PathVariable("userId")Long userId,
@@ -41,6 +45,7 @@ public class MeetingController {
         return ResponseEntity.ok(true);
     }
 
+    @Schema(name = "Search Meeting", description = "회의록 검색 API (회원 ID)")
     @GetMapping("/search/{userId}")
     public ResponseEntity<List> searchMeetingByUserId(@PathVariable("userId") Long userId,
                                                       HttpServletRequest request){
@@ -55,6 +60,7 @@ public class MeetingController {
         return ResponseEntity.ok(findMeetings);
     }
 
+    @Schema(name = "Delete Meeting", description = "회의록 삭제 API (회의록 ID)")
     @PostMapping("/delete/{meetingId}")
     public ResponseEntity<Boolean> deleteMeetingByMeetingId(@PathVariable("meetingId") Long id,
                                                             HttpServletRequest request){
@@ -65,6 +71,23 @@ public class MeetingController {
         tokenManager.validateToken(accessToken);
 
         meetingService.deleteMeeting(id);
+
+        return ResponseEntity.ok(true);
+    }
+
+    @Schema(name = "Delete All Meeting", description = "회의록 삭제 API (회원의 전체 회의록)")
+    @PostMapping("/delete/{userId}")
+    public ResponseEntity<Boolean> deleteAllMeetingByUserId(@PathVariable("userId") Long userId,
+                                                            HttpServletRequest request){
+
+        String authorization = request.getHeader("Authorization");
+        String accessToken = authorization.split(" ")[1];
+
+        tokenManager.validateToken(accessToken);
+
+        User findUser = userService.findUserByUserId(userId);
+
+        findUser.getMeetings().clear();
 
         return ResponseEntity.ok(true);
     }
