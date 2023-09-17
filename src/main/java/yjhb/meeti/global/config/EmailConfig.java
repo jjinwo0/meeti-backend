55327459cbb3035ac0e32a6c4e6ae97f281f23b4
Jinwoo <zvzv9808@gmail.com>
2023-09-18 07:56:54 +0900

    refactor: modify email config

--- src/main/java/yjhb/meeti/global/config/EmailConfig.java
+++ src/main/java/yjhb/meeti/global/config/EmailConfig.java
@@ -1,27 +1,55 @@
 package yjhb.meeti.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
 @PropertySource("classpath:email.properties")
 public class EmailConfig {
 
-    @Value("${spring.mail.port}")
+    @Value("${spring.port}")
     private int port;
-    @Value("${spring.mail.properties.mail.smtp.socketFactory.port}")
+    @Value("${spring.properties.mail.smtp.socketFactory.port}")
     private int socketPort;
-    @Value("${spring.mail.properties.mail.smtp.auth}")
+    @Value("${spring.properties.mail.smtp.auth}")
     private boolean auth;
-    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    @Value("${spring.properties.mail.smtp.starttls.enable}")
     private boolean starttls;
-    @Value("${spring.mail.properties.mail.smtp.starttls.required}")
+    @Value("${spring.properties.mail.smtp.starttls.required}")
     private boolean startlls_required;
-    @Value("${spring.mail.properties.mail.smtp.socketFactory.fallback}")
+    @Value("${spring.properties.mail.smtp.socketFactory.fallback}")
     private boolean fallback;
-    @Value("${spring.mail.username}")
+    @Value("${spring.username}")
     private String id;
     @Value("${spring.mail.password}")
     private String password;
+
+    @Bean
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setUsername(id);
+        javaMailSender.setPassword(password);
+        javaMailSender.setPort(port);
+        javaMailSender.setJavaMailProperties(getMailProperties());
+        javaMailSender.setDefaultEncoding("UTF-8");
+        return javaMailSender;
+    }
+    private Properties getMailProperties()
+    {
+        Properties pt = new Properties();
+        pt.put("mail.smtp.socketFactory.port", socketPort);
+        pt.put("mail.smtp.auth", auth);
+        pt.put("mail.smtp.starttls.enable", starttls);
+        pt.put("mail.smtp.starttls.required", startlls_required);
+        pt.put("mail.smtp.socketFactory.fallback",fallback);
+        pt.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        return pt;
+    }
 }
