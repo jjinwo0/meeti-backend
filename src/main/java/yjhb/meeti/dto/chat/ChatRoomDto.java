92d97d9d10b16c4ec261a9733b9fa2ab72296f3d
Jinwoo <zvzv9808@gmail.com>
2023-10-17 19:41:17 +0900

    refactor: modify ChatRoomService

--- src/main/java/yjhb/meeti/dto/chat/ChatRoomDto.java
+++ src/main/java/yjhb/meeti/dto/chat/ChatRoomDto.java
@@ -3,18 +3,29 @@ package yjhb.meeti.dto.chat;
 import lombok.Data;
 import yjhb.meeti.domain.chat.ChatRoom;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class ChatRoomDto {
 
     @Data
-    public static class Request{
+    public static class CreateRequest{
 
         private String roomName;
 
-        public ChatRoom toEntity(){
+        @Min(2) @Max(50)
+        private int maxCount;
 
-            return ChatRoom.builder()
-                    .roomName(this.roomName)
-                    .build();
-        }
+        @NotNull
+        Long userId;
+    }
+
+    @Data
+    public static class JoinRequest{
+
+        private Long roomId;
+
+        private Long joinUserId;
     }
 }
--- src/main/java/yjhb/meeti/service/chat/ChatRoomService.java
+++ src/main/java/yjhb/meeti/service/chat/ChatRoomService.java
@@ -5,8 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import yjhb.meeti.domain.chat.ChatRoom;
+import yjhb.meeti.domain.user.entity.User;
 import yjhb.meeti.dto.chat.ChatRoomDto;
+import yjhb.meeti.global.error.ErrorCode;
+import yjhb.meeti.global.error.exception.EntityNotFoundException;
 import yjhb.meeti.repository.chat.ChatRoomRepository;
+import yjhb.meeti.service.user.UserService;
 
 import java.util.List;
 
@@ -17,11 +21,32 @@ import java.util.List;
 public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
+    private final UserService userService;
 
     @Transactional
-    public Long createRoom(ChatRoomDto.Request request){
+    public Long createRoom(ChatRoomDto.CreateRequest request){
 
-        return chatRoomRepository.save(request.toEntity()).getId();
+        ChatRoom createRoom = ChatRoom.builder()
+                .admin(userService.findUserByUserId(request.getUserId()))
+                .maxCount(request.getMaxCount())
+                .roomName(request.getRoomName())
+                .build();
+
+        return chatRoomRepository.save(createRoom).getId();
+    }
+
+    public void joinRoom(ChatRoomDto.JoinRequest request){
+
+        ChatRoom findRoom = findByRoomId(request.getRoomId());
+        User joinUser = userService.findUserByUserId(request.getJoinUserId());
+
+        findRoom.addUser(joinUser);
+    }
+
+    public ChatRoom findByRoomId(Long roomId){
+
+        return chatRoomRepository.findById(roomId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.NOT_FOUNT_ROOM));
     }
 
     public List<ChatRoom> findByRoomName(String roomName){
